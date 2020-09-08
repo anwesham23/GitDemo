e8b4b802ee16a09d54f50474cd299c85fce417cd
anwesha <anwesham23@gmail.com>
2020-09-08 12:39:27 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+@Test
+public void postJira()
+{
+	System.out.println("Post Jira");
+}
+@Test
+public void deleteTwitter()
+{
+	System.out.println("Delete Twitter");
+}
+}
